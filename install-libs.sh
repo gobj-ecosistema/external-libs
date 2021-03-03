2bf14b91afb8fb4d740586fe68beb31b5c7ff8e4
Niyamaka <niyamaka@yuneta.io>
2021-03-03 08:17:38 +0100

    update to OpenSSL 1.1.1j

--- install-libs.sh
+++ install-libs.sh
@@ -3,7 +3,7 @@
 #------------------------------------------
 #   openssl
 #------------------------------------------
-cd build/openssl-OpenSSL_1_1_1i
+cd build/openssl-OpenSSL_1_1_1j
 make install
 cd ../..
 
