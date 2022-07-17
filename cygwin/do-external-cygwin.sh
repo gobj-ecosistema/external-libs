6593dcae1b37d55f487128754586158f97df2dae
ginsmar <ginsmar@gmail.com>
2022-07-17 17:03:46 +0200

    automatiza cygwin

---
+++ cygwin/do-external-cygwin.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#  Exit immediately if a command exits with a non-zero status.
+set -e
+
+cd /yuneta/development/yuneta/^gobj-ecosistema/external-libs
+
+rm -rf build/
+mkdir build
+cd build
+
+echo "extrae jannson"
+tar xzf ../sources/jansson-2.13.1.tar.gz
+
+echo "extrae libuv"
+tar xzf ../sources/libuv-1.44.2.gines.tar.gz
+
+echo "extrae pcre2"
+tar xzf ../sources/pcre2-10.37.tar.gz
+
+cd ..
+
+#------------------------------------------
+#   Jansson
+#------------------------------------------
+echo "===================== JANSSON ======================="
+cd build/jansson-2.13.1
+
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX:PATH=/yuneta/development/output ..
+make install
+cd ../../..
+
+#------------------------------------------
+#   Libuv
+#------------------------------------------
+echo "===================== LIBUV ======================="
+cd build/libuv-1.44.2.gines
+sh autogen.sh
+./configure --prefix=/yuneta/development/output
+make
+make install
+cd ../..
+
+#------------------------------------------
+#   PCRE
+#------------------------------------------
+# HACK WARNING en redhat usa ./configure
+cd build/pcre2-10.37
+./configure --prefix=/yuneta/development/output --enable-jit
+make
+make install
+cd ../..
