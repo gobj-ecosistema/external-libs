662aed20e0dff18d8562ea789d3daea322f2a520
Niyamaka <niyamaka@yuneta.io>
2022-07-22 13:45:44 +0200

    merde

--- windows/do-external-windows.sh
+++ windows/do-external-windows.sh
@@ -5,16 +5,16 @@ set -e
 
 cd /c/yuneta/development/yuneta/^gobj-ecosistema/external-libs
 
-rm -rf build/
-mkdir build
-cd build
+#rm -rf build/
+#mkdir build
+#cd build
 
-echo "extrae jannson"
-tar xzf ../sources/jansson-2.13.1.tar.gz
+#echo "extrae jannson"
+#tar xzf ../sources/jansson-2.13.1.tar.gz
 
-#echo "extrae libuv"
+##echo "extrae libuv"
 #tar xzf ../sources/libuv-1.44.2.gines.tar.gz
-#
+
 #echo "extrae pcre2"
 #tar xzf ../sources/pcre2-10.40.tar.gz
 #
@@ -35,23 +35,20 @@ tar xzf ../sources/jansson-2.13.1.tar.gz
 
 cd ..
 
-#export CFLAGS="-Wno-error=char-subscripts -O0 -g3 -ggdb"
-
 #------------------------------------------
 #   Jansson
-# To compile: open build/jansson-2.13.1 with clion
-#   and set in CMake options:
-#     -DCMAKE_INSTALL_PREFIX:PATH=c:/yuneta/development/output
-# (the generared sln project cannot be open well with Visual Studio 2022)
-#------------------------------------------
-#echo "===================== JANSSON ======================="
-#cd build/jansson-2.13.1
+#   To compile: open build/jansson-2.13.1 with clion
+#     and set in CMake options:
+#       -DCMAKE_INSTALL_PREFIX:PATH=c:/yuneta/development/output
+#   (the generared sln project cannot be open well with Visual Studio 2022)
+#   And add to CMakeLists.txt the line:
 #
-#mkdir build
-#cd build
-#cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX:PATH=c:/yuneta/development/output ..
-#make install
-#cd ../../..
+#set(JANSSON_HDR_PUBLIC
+#   ${CMAKE_CURRENT_BINARY_DIR}/include/jansson_config.h
+#   ${CMAKE_CURRENT_BINARY_DIR}/include/jansson_private.h
+#   ${CMAKE_CURRENT_SOURCE_DIR}/src/jansson.h)
+#------------------------------------------
+
 
 #------------------------------------------
 #   Libuv
