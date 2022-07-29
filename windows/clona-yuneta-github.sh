a7670e9687d7e0fa999c1d575c02ccb2765c3d4e
ginsmar <ginsmar@gmail.com>
2022-07-29 09:06:59 +0200

    command to clone in windows

---
+++ windows/clona-yuneta-github.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+git clone git@github.com:gobj-ecosistema/external-libs.git /c/yuneta/development/yuneta/^gobj-ecosistema/external-libs
+git clone git@github.com:gobj-ecosistema/ghelpers.git /c/yuneta/development/yuneta/^gobj-ecosistema/ghelpers
+git clone git@github.com:gobj-ecosistema/ginsfsm.git /c/yuneta/development/yuneta/^gobj-ecosistema/ginsfsm
+git clone git@github.com:gobj-ecosistema/tests-g.git /c/yuneta/development/yuneta/^gobj-ecosistema/tests-g
+git clone git@github.com:gobj-ecosistema/stats.git /c/yuneta/development/yuneta/^gobj-ecosistema/stats
+git clone git@github.com:gobj-ecosistema/timeranger.git /c/yuneta/development/yuneta/^gobj-ecosistema/timeranger
+git clone git@github.com:gobj-ecosistema/ytls.git /c/yuneta/development/yuneta/^gobj-ecosistema/ytls
+git clone git@github.com:gobj-ecosistema/rxspencer.git /c/yuneta/development/yuneta/^gobj-ecosistema/rxspencer
+
+################################################
+#   Clone yuneta
+################################################
+git clone git@github.com:yuneta/c-core /c/yuneta/development/yuneta/^yuneta/c-core
+git clone git@github.com:yuneta/c-rc_sqlite /c/yuneta/development/yuneta/^yuneta/c-rc_sqlite
+git clone git@github.com:yuneta/js-core /c/yuneta/development/yuneta/^yuneta/js-core
+git clone git@github.com:yuneta/c-tls /c/yuneta/development/yuneta/^yuneta/c-tls
+git clone git@github.com:yuneta/c-postgres /c/yuneta/development/yuneta/^yuneta/c-postgres
+git clone git@github.com:yuneta/c-iot /c/yuneta/development/yuneta/^yuneta/c-iot
+git clone git@github.com:yuneta/packages /c/yuneta/development/yuneta/^yuneta/packages
+git clone git@github.com:yuneta/tests-y /c/yuneta/development/yuneta/^yuneta/tests-y
+git clone git@github.com:yuneta/web-skeleton3 /c/yuneta/development/yuneta/^yuneta/web-skeleton3
+git clone git@github.com:yuneta/ybatch /c/yuneta/development/yuneta/^yuneta/ybatch
+git clone git@github.com:yuneta/ycommand /c/yuneta/development/yuneta/^yuneta/ycommand
+git clone git@github.com:yuneta/ylist /c/yuneta/development/yuneta/^yuneta/ylist
+git clone git@github.com:yuneta/yshutdown /c/yuneta/development/yuneta/^yuneta/yshutdown
+git clone git@github.com:yuneta/ystats /c/yuneta/development/yuneta/^yuneta/ystats
+git clone git@github.com:yuneta/ytestconfig /c/yuneta/development/yuneta/^yuneta/ytestconfig
+git clone git@github.com:yuneta/yuno_agent /c/yuneta/development/yuneta/^yuneta/yuno_agent
+git clone git@github.com:yuneta/yuno_cli /c/yuneta/development/yuneta/^yuneta/yuno_cli
+git clone git@github.com:yuneta/yuno-skeleton /c/yuneta/development/yuneta/^yuneta/yuno-skeleton
+git clone git@github.com:yuneta/ygclass-rename /c/yuneta/development/yuneta/^yuneta/ygclass-rename
+git clone git@github.com:yuneta/yscapec /c/yuneta/development/yuneta/^yuneta/yscapec
+git clone git@github.com:yuneta/mxgraph-js.git /c/yuneta/development/yuneta/^yuneta/mxgraph-js
+git clone git@github.com:yuneta/ytests /c/yuneta/development/yuneta/^yuneta/ytests
+git clone git@github.com:yuneta/extractjson /c/yuneta/development/yuneta/^yuneta/extractjson
+git clone git@github.com:yuneta/yclone-project /c/yuneta/development/yuneta/^yuneta/yclone-project/
+git clone git@github.com:yuneta/yuno_agent22 /c/yuneta/development/yuneta/^yuneta/yuno_agent22/
+
+################################################
+#   Clone yuno-store
+################################################
+git clone git@github.com:yuno-store/emailsender /c/yuneta/development/yuneta/^yunos/emailsender
+git clone git@github.com:yuno-store/emu_device /c/yuneta/development/yuneta/^yunos/emu_device
+git clone git@github.com:yuno-store/logcenter /c/yuneta/development/yuneta/^yunos/logcenter
+git clone git@github.com:yuno-store/watchfs /c/yuneta/development/yuneta/^yunos/watchfs
+git clone git@github.com:yuno-store/controlcenter /c/yuneta/development/yuneta/^yunos/controlcenter
+git clone git@github.com:yuno-store/sgateway /c/yuneta/development/yuneta/^yunos/sgateway
+git clone git@github.com:yuno-store/dba_postgres /c/yuneta/development/yuneta/^yunos/dba_postgres
+
+
+################################################
+#   Windows
+################################################
+git clone git@github.com:niyamaka/windows.git /c/yuneta/projects/windows
